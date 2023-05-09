489f61f1bb90cdef6068e4e9fd282f2218659d82
Devi Amalia <73010431+deviam7@users.noreply.github.com>
2023-05-09 20:24:41 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-    name := "Go Developers"
-    fmt.Println("Azure for", name)
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1312"))
 }
